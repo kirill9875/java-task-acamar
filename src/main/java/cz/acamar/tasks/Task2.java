e62e350c9da8d624ea4b5cfabaf81c807e42cca3
Kirill Bakurkin <kirill1098@gmail.com>
2022-05-10 17:12:30 +0300

    Task 2 ready

--- src/main/java/cz/acamar/tasks/Task2.java
+++ src/main/java/cz/acamar/tasks/Task2.java
@@ -1,5 +1,7 @@
 package cz.acamar.tasks;
 
+import java.util.Arrays;
+
 public class Task2 {
 
     /**
@@ -18,6 +20,6 @@ public class Task2 {
      * @return - an array of the squares of each number sorted in non-decreasing order
      */
     public int[] squaresOfSortedArray(int[] input) {
-        return null;
+        return Arrays.stream(input).map(m -> m * m).sorted().toArray();
     }
 }
